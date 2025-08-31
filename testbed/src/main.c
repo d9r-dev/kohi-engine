584380a40d279c2f6ea1377bea18f42d2c5f1d20
Danny Krämer <d.kraemer@dotsource.de>
2025-08-31 19:58:21 +0200

    add windows platform layer

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,9 +1,23 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+// TODO: Test
+#include <platform/platform.h>
+
 int main()
 {
-  KFATAL("Fatal message. %f", 3.14f);
-  KASSERT(1 == 2);
-  return 0;
+    KERROR("Test");
+    KINFO("Test");
+    platform_state state;
+    if (platform_startup(&state, "Engine Testbed", 100, 100, 1200, 720))
+    {
+        while (TRUE)
+        {
+            platform_pump_messages(&state);
+        }
+    }
+
+    platform_shutdown(&state);
+
+    return 0;
 }
